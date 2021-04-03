3bde7f705736d8b500e43591d3b1566c9f23f34b
Max Huckstepp <mhuckstepp@gmail.com>
2021-04-02 20:42:39 -0700

    initial commit

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,57 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Max Huckstepp', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  description: 'Hello!', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  name: 'Max Huckstepp',
+  subtitle: 'Software Developer',
+  cta: 'Learn more',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    'Max is a former enterprise software sales and customer facing engineer turned software developer. His interests include reading, running and meditation.',
+  paragraphTwo:
+    "Max likes to learn new things and play with tech, he has tried almost every smart home gadget. He also enjoys flying RC planes and drones and is currently trying to pick up the harmonica (it's not going well)",
+  resume: 'https://www.linkedin.com/in/max-huckstepp/', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'notxkcd.jpg',
+    title: 'notxkcd Web App',
+    info:
+      'As a big fan of xkcd comics, I thought the xkcd website was lacking and decided to build my own. Check it out!',
+    info2: 'This project utilized react, redux, tailwind.css',
+    url: 'https://notxkcd.vercel.app/',
+    repo: 'https://github.com/mhuckstepp/notxkcd', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'plants.jpg',
+    title: 'Water My Plant',
+    info: 'I built this app to help people remember to water their plants on the right schedule.',
+    info2: ' Built with React and Redux',
+    url: 'https://frontend-phi-ivory.vercel.app/myplants',
+    repo: 'https://github.com/mhuckstepp/WaterMyPlants', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: "Let's talk",
+  btn: 'contact me',
+  email: 'mhuckstepp@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,27 +61,23 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/home',
     },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
+
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/max-huckstepp/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/mhuckstepp',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Max Huckstepp'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Max Huckstepp'} />
       </Helmet>
       <App />
     </>
