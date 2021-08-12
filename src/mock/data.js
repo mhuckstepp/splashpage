d390bc39dfc73894c01c5b95fc4a325820ac73ce
Max Huckstepp <mhuckstepp@gmail.com>
2021-08-12 14:48:58 -0700

    copy changes

--- src/mock/data.js
+++ src/mock/data.js
@@ -50,7 +50,7 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'plants.jpeg',
-    title: 'Water My Plant',
+    title: 'Water My Plants',
     info:
       'I built this app to help people remember to water their plants at the right time. The site checks local weather to update watering schedule when it rains and sends email notifications to users to water.',
     info2:
